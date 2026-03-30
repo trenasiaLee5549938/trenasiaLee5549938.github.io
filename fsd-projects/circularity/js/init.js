2248f1c2db5566a2018b0206f9cd74662b70d5e0
trenasiaLee5549938 <tll5549938@lpssonline.com>
2026-03-30 14:51:32 +0000

    todo 2 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,11 +20,21 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+var circle
+var circles = []
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {  circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+
+              physikz.addRandomVelocity(circle, canvas, 2, 2);
+
+              view.addChild(circle);
+
+              circles.push(circle);
+
+       
+            
+        }
 
 
         // TODO 3 : Call the drawCircle() function
