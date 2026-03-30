61216481f6dd816d2627c66214ded6c39ed701d8
trenasiaLee5549938 <tll5549938@lpssonline.com>
2026-03-30 15:01:19 +0000

    todo 7 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -45,7 +45,20 @@ drawCircle()
 drawCircle()
 
         // TODO 7 : Use a loop to create multiple circles
+drawCircle()
 
+drawCircle()
+
+drawCircle()
+
+drawCircle()
+
+drawCircle()
+for (var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++) {
+
+            drawCircle();
+
+        }
 
 
 
