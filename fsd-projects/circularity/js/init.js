cc0b126d1d65055fbbdf8d979b59fd9a07edbf69
trenasiaLee5549938 <tll5549938@lpssonline.com>
2026-03-30 14:59:57 +0000

    todo 6 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -99,7 +99,13 @@ game.checkCirclePosition(circles[ 4 ])
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            else if(circle.x < 0) {
+                circle.x = canvas.width;
+            } else if(circle.y > canvas.height) {
+                circle.y = 0;
+            } else if(circle.y <0) {
+                circle.y = canvas.height;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
